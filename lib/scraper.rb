e9ddc9248e3ed3733e9a902e139a823305a6c3d3
Anthony Lepore <violacellonyc@gmail.com>
2020-08-16 19:53:52 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,24 +9,20 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     
-   
     html = open(index_url)
     doc = Nokogiri::HTML(html)
-#   binding.pry
+# binding.pry
     doc.css(".student-card").each do |i|
-     
-     temp_hash = {}
+       temp_hash = {}
    
-  name = i.css("h4").text.strip
-  location = i.css("p").text.strip
-  #profile_url = i.css(".student-card a").values 
-  temp_hash[:name] = name
-  temp_hash[:location] = location
-  #temp_hash[:profile_url] = profile_url
+      name = i.css("h4").text.strip
+      location = i.css("p").text.strip
+      profile_url = i.css("a").attr('href').text 
+          temp_hash[:name] = name
+          temp_hash[:location] = location
+          temp_hash[:profile_url] = profile_url
   @@scraped_students.push(temp_hash)
-
-end
-  binding.pry
+      end
   @@scraped_students
   end
 
