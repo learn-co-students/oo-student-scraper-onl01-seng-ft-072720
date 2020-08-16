242da83bc49bdcb8031d2a68aca6ec002318b47b
Anthony Lepore <violacellonyc@gmail.com>
2020-08-16 21:15:13 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,27 +28,28 @@ class Scraper
   end # ends method 
 
  def self.scrape_profile_page(profile_url)
+   scraped_profile = {}
    html = open(profile_url)
     doc = Nokogiri::HTML(html)
     s_media = []
     doc.css(".social-icon-container a").each do |i|
      s_media.push(i)
    end
-   s_media.each do |i|
-     @@scraped_profile[:twitter] = i.attr('href') if i.attr('href').include?("twitter")
-     @@scraped_profile[:linkedin] = i.attr('href') if i.attr('href').include?("linkedin")
-    @@scraped_profile[:github] = i.attr('href') if i.attr('href').include?("github")
-     @@scraped_profile[:blog] = i.attr('href') if i.attr('href').inlude?("blog")
-    
-      
-      
-    
-     end
-   #end
-   binding.pry
+   i = 0 
+   j = s_media.length
    
- 
-   
+   while i < j-1 do 
+     scraped_profile[:twitter] = s_media[i].attr('href') if s_media[i].attr('href').include?("twitter")
+     scraped_profile[:linkedin] = s_media[i].attr('href') if s_media[i].attr('href').include?("linkedin")
+    scraped_profile[:github] = s_media[i].attr('href') if s_media[i].attr('href').include?("github")
+     scraped_profile[:blog] = s_media[i].attr('href') if (i == j-1) && j-1 > scraped_profile.length
+     i += 1
+   end
+  
+    scraped_profile[:profile_quote] = doc.css(".profile-quote").text
+    scraped_profile[:bio] = doc.css("div.description-holder p").text
+    scraped_profile
+    #binding.pry
  end # ends method 
    
   
