088632d3203a667ade5ee516492e46e6acb05d95
Anthony Lepore <violacellonyc@gmail.com>
2020-08-16 19:33:21 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,14 +2,32 @@ require 'open-uri'
 require 'pry'
 require 'nokogiri'
 
+
 class Scraper
 
+@@scraped_students = []
+
   def self.scrape_index_page(index_url)
+    
+   
     html = open(index_url)
     doc = Nokogiri::HTML(html)
-  
+#   binding.pry
+    doc.css(".student-card").each do |i|
+     
+     temp_hash = {}
+   
+  name = i.css("h4").text.strip
+  location = i.css("p").text.strip
+  #profile_url = i.css(".student-card a").values 
+  temp_hash[:name] = name
+  temp_hash[:location] = location
+  #temp_hash[:profile_url] = profile_url
+  @@scraped_students.push(temp_hash)
+
+end
   binding.pry
-  
+  @@scraped_students
   end
 
   def self.scrape_profile_page(profile_url)
@@ -17,4 +35,3 @@ class Scraper
   end
 
 end
-
